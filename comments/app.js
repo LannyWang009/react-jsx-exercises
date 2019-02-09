be55148dbe07664592bfee94abba21b5bd0ccb1b
LannyWang009 <09legend.wang@gmail.com>
2019-02-09 15:15:54 -0600

    made new logo and updated comments

--- comments/app.js
+++ comments/app.js
@@ -1,17 +1,23 @@
 let ProfilePic = () => {
-    return <span>Make this component render an image</span>
+  return <span>Make this component render an image</span>
 }
 
 let CommentBody = () => {
-    return <span>Make this component render a username in bold above the comment text</span>
+  return <span>Make this component render a username in bold above the comment text</span>
 }
 
 let Comment = () => {
-    return <span>Make each comment render a profile picture and the comment body</span>
+  return <span>Make each comment render a profile picture and the comment body</span>
 }
 
 let Comments = () => {
-    return <span>Make this component render a list of comments</span>
+  return (
+    <div>
+      <ProfilePic />
+      <CommentBody />
+      <Comment />
+    </div>
+  )
 }
 
 ReactDOM.render(<Comments />, document.getElementById('root'))
